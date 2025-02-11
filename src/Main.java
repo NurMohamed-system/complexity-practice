69ae015a0f5ea964697e76649cc710f3cceee56f
Nur Mohamed <mohamed.nur@student.greenriver.edu>
2025-02-10 20:47:15 -0800

    Completed

--- src/Main.java
+++ src/Main.java
@@ -5,196 +5,205 @@ import java.util.Set;
 
 public class Main {
 
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static void timesTable(int x) {
-    for(int i = 1; i <= x; i++) {
-        for(int j = 1; j <= x; j++) {
-            System.out.print(i*j + " ");
-        }
-        System.out.println();
-    }
-  }
-
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static void printLetters(String word) {
-    char[] letters = word.toCharArray();
-
-    for (char letter : letters) {
-        System.out.println(letter);
-    }
-  }
-
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static boolean isBanned(String password) {
-    String[] bannedPasswords = {"password", "hello", "qwerty"};
-    boolean banned = false;
-    for(String bannedPassword : bannedPasswords) {
-        if(password.equals(bannedPassword)) {
-            banned = true;
-        }
-    }
-    return banned;
-  }
-
-
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static int computeProduct(int[] nums) {
-    int total = 1;
-    for(int num : nums) {
-        total *= num;
-    }
-    return total;
-  }
-
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static void describeProduct(int[] nums) {
-    System.out.println("About to compute the product of the array...");
-    int product = computeProduct(nums);
-    System.out.println("The product I found was " + product);
-  }
-
-
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static int computeFactorial(int n) {
-    int result = 1;
-    for(int i = 1; i <= n; i++) {
-        result *= n;
-    }
-    return result;
-  }
-
-  // Assume that the largest number is no bigger than the length
-  // of the array
-  public static void computeAllFactorials(int[] nums) {
-    for(int num : nums) {
-        int result = computeFactorial(num);
-        System.out.println("The factorial of " + num + " is " + result);
-    }
-  }
-
-
-  // assume that each String is bounded by a constant length
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static void checkIfContainedArrayList(ArrayList<String> arr, String target) {
-    if (arr.contains(target)) {
-        System.out.println(target + " is present in the list");
-    } else {
-        System.out.println(target + " is not present in the list");
-    }
-  }
-
-
-  // assume n = wordsA.length = wordsB.length
-  // assume that each String is bounded by a constant length
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static boolean containsOverlap(String[] wordsA, String[] wordsB) {
-    for(String wordA : wordsA) {
-        for(String wordB : wordsB) {
-            if(wordA.equals(wordB)) {
-                return true;
-            }
-        }
-    }
-    return false;
-  }
-
-  // assume that each String is bounded by a constant length
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static boolean containsOverlap2(String[] wordsA, String[] wordsB) {
-    Set<String> wordsSet = new HashSet<>();
-    for(String word : wordsA) {
-        wordsSet.add(word);
-    }
-
-    for(String word : wordsB) {
-        if(wordsSet.contains(word)) {
-            return true;
-        }
-    }
-
-    return false;
-  }
-
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static void printCharacters(char[] chars) {
-    for (int i = 0; i < chars.length; i++) {
-      char character = chars[i];
-      System.out.println("The character at index " + i + " is " + character);
-    }
-  }
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static double computeAverage(double a, double b) {
-    return (a + b) / 2.0;
-  }
-
-  // assume that each String is bounded by a constant length
-  // The time complexity is:
-  // YOUR ANSWER HERE
-  public static void checkIfContainedHashSet(HashSet<String> set, String target)
-  {
-    if (set.contains(target)) {
-      System.out.println(target + " is present in the set");
-    } else {
-      System.out.println(target + " is not present in the set");
-    }
-  }
-
-  // emailLookup attempts to find the email associated with a name.
-  // The name at index i of names corresponds to the email at index i of emails
-  // A queryName is given, and this method returns the corresponding email if it is found
-  // Otherwise, it returns "Person not found"
-  // assume that each String is bounded by a constant length
-  // What is the time complexity of this method?
-  // YOUR ANSWER HERE
-  public static String emailLookup(String[] names, String[] emails, String queryName) {
-    for(int i = 0; i < names.length; i++) {
-      if (names[i].equals(queryName)) {
-        return emails[i];
+  private static ArrayList<String> list; // added this line, to answer the 'hasCommonEfficient' question
+    // The time complexity is:
+    // o(n^2)  n is multiply n 
+    public static void timesTable(int x) {
+      for(int i = 1; i <= x; i++) {
+          for(int j = 1; j <= x; j++) {
+              System.out.print(i*j + " ");
+          }
+          System.out.println();
       }
     }
-    return "Person not found";
-  }
-
-  // Suppose that emailLookupEfficient performs the same task as emailLookup
-  // However, instead of two arrays it is passed a map where the
-  // keys are names and the values are emails.
-  // Write this method to efficiently return the corresponding email or "Person not found" if appropriate
-  // assume that each String is bounded by a constant length
-  // What is the time complexity of your solution?
-  // YOUR ANSWER HERE
-  public static String emailLookupEfficient(HashMap<String, String> namesToEmails, String queryName) {
-    return null;
-  }
-
-  // What is the time complexity of this method?
-  // assume that each String is bounded by a constant length
-  // (assume the set and list have the same number of elements)
-  // YOUR ANSWER HERE
-  public static boolean hasCommon(HashSet<String> wordSet, ArrayList<String> wordList) {
-    for(String word : wordSet) {
-      if(wordList.contains(word)) {
-        return true;
+  
+    // The time complexity is:
+    // 0(n) n is the letter
+  
+    public static void printLetters(String word) {
+      char[] letters = word.toCharArray();
+  
+      for (char letter : letters) {
+          System.out.println(letter);
       }
     }
-    return false;
-  }
-  // Rewrite hasCommon so it does the same thing as hasCommon, but with a better time complexity.
-  // Do not change the datatype of wordSet or wordList.
-  // assume that each String is bounded by a constant length
-  // What is the time complexity of your new solution?
-  // YOUR ANSWER HERE
-  public static boolean hasCommonEfficient(HashSet<String> wordSet, ArrayList<String> wordList) {
+  
+    // The time complexity is:
+    // 0(n) n is bannedPassword
+    public static boolean isBanned(String password) {
+      String[] bannedPasswords = {"password", "hello", "qwerty"};
+      boolean banned = false;
+      for(String bannedPassword : bannedPasswords) {
+          if(password.equals(bannedPassword)) {
+              banned = true;
+          }
+      }
+      return banned;
+    }
+  
+  
+    // The time complexity is:
+    // o(n) n is number
+    public static int computeProduct(int[] nums) {
+      int total = 1;
+      for(int num : nums) {
+          total *= num;
+      }
+      return total;
+    }
+  
+    // The time complexity is:
+    // 0(1) constant
+    public static void describeProduct(int[] nums) {
+      System.out.println("About to compute the product of the array...");
+      int product = computeProduct(nums);
+      System.out.println("The product I found was " + product);
+    }
+  
+  
+    // The time complexity is:
+    // o(n) n is num
+    public static int computeFactorial(int n) {
+      int result = 1;
+      for(int i = 1; i <= n; i++) {
+          result *= n;
+      }
+      return result;
+    }
+  
+    // Assume that the largest number is no bigger than the length
+    // of the array
+    public static void computeAllFactorials(int[] nums) {
+      for(int num : nums) {
+          int result = computeFactorial(num);
+          System.out.println("The factorial of " + num + " is " + result);
+      }
+    }
+  
+  
+    // assume that each String is bounded by a constant length
+    // The time complexity is:
+    // O(n) n is string
+    public static void checkIfContainedArrayList(ArrayList<String> arr, String target) {
+      if (arr.contains(target)) {
+          System.out.println(target + " is present in the list");
+      } else {
+          System.out.println(target + " is not present in the list");
+      }
+    }
+  
+  
+    // assume n = wordsA.length = wordsB.length
+    // assume that each String is bounded by a constant length
+    // The time complexity is:
+    // O(n2) n is length
+    public static boolean containsOverlap(String[] wordsA, String[] wordsB) {
+      for(String wordA : wordsA) {
+          for(String wordB : wordsB) {
+              if(wordA.equals(wordB)) {
+                  return true;
+              }
+          }
+      }
+      return false;
+    }
+  
+    // assume that each String is bounded by a constant length
+    // The time complexity is:
+    // o(n) n is length
+    public static boolean containsOverlap2(String[] wordsA, String[] wordsB) {
+      Set<String> wordsSet = new HashSet<>();
+      for(String word : wordsA) {
+          wordsSet.add(word);
+      }
+  
+      for(String word : wordsB) {
+          if(wordsSet.contains(word)) {
+              return true;
+          }
+      }
+  
+      return false;
+    }
+  
+    // The time complexity is:
+    // O(n) n is the length
+    public static void printCharacters(char[] chars) {
+      for (int i = 0; i < chars.length; i++) {
+        char character = chars[i];
+        System.out.println("The character at index " + i + " is " + character);
+      }
+    }
+    // The time complexity is:
+    // O(1) constant
+    public static double computeAverage(double a, double b) {
+      return (a + b) / 2.0;
+    }
+  
+    // assume that each String is bounded by a constant length
+    // The time complexity is:
+    // O(1) constant
+    public static void checkIfContainedHashSet(HashSet<String> set, String target)
+    {
+      if (set.contains(target)) {
+        System.out.println(target + " is present in the set");
+      } else {
+        System.out.println(target + " is not present in the set");
+      }
+    }
+  
+    // emailLookup attempts to find the email associated with a name.
+    // The name at index i of names corresponds to the email at index i of emails
+    // A queryName is given, and this method returns the corresponding email if it is found
+    // Otherwise, it returns "Person not found"
+    // assume that each String is bounded by a constant length
+    // What is the time complexity of this method?
+    // O(n) n is length
+    public static String emailLookup(String[] names, String[] emails, String queryName) {
+      for(int i = 0; i < names.length; i++) {
+        if (names[i].equals(queryName)) {
+          return emails[i];
+        }
+      }
+      return "Person not found";
+    }
+  
+    // Suppose that emailLookupEfficient performs the same task as emailLookup
+    // However, instead of two arrays it is passed a map where the
+    // keys are names and the values are emails.
+    // Write this method to efficiently return the corresponding email or "Person not found" if appropriate
+    // assume that each String is bounded by a constant length
+    // What is the time complexity of your solution?
+    // O(1) constant
+    public static String emailLookupEfficient(HashMap<String, String> namesToEmails, String queryName) {
+      return null;
+    }
+  
+    // What is the time complexity of this method?
+    // assume that each String is bounded by a constant length
+    // (assume the set and list have the same number of elements)
+    // O(n2) n is length
+    public static boolean hasCommon(HashSet<String> wordSet, ArrayList<String> wordList) {
+      for(String word : wordSet) {
+        if(wordList.contains(word)) {
+          return true;
+        }
+      }
+      return false;
+    }
+    // Rewrite hasCommon so it does the same thing as hasCommon, but with a better time complexity.
+    // Do not change the datatype of wordSet or wordList.
+    // assume that each String is bounded by a constant length
+    // What is the time complexity of your new solution?
+    // O(n3)  n is length
+    public static boolean hasCommonEfficient(HashSet<String> wordSet, ArrayList<String> wordList) {
+      for(String word : wordSet) {
+        if(wordList.contains(word)) {
+          return true;
+        } else {
+          list.add(0, "Fish");
+      }
+    }
     return false;
   }
 
@@ -203,14 +212,14 @@ public class Main {
   // The prices will be updated frequently throughout the day, and you need to efficiently update
   // and access the current price for each stock. The order of the ticker symbols is not important.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
+  // HashMap
 
   // Suppose you are building a music player application where users can create playlists.
   // Songs can be added to the end of the playlist in the order the user chooses, and the user can
   // skip to the next or previous song. Most operations involve adding songs and accessing them by
   // their position in the playlist.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
+  // ArrayList
 
   // Suppose you are developing a search feature that keeps track of the user's
   // recent search queries. You want to store the queries in the order they were made,
@@ -218,5 +227,7 @@ public class Main {
   // relatively small, and it is more important to preserve the order of the searches than
   // to optimize for fast lookups or deletions.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
+  // TreeMap
+
+
 }
